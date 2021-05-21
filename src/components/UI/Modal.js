53d1239e7546e48338c68783cf4c64bb8b24ad16
Ana MacBook <anac2828@yahoo.com>
2021-05-20 19:13:45 -0700

    finished open and close cart

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,40 +4,44 @@ import styles from './Modal.module.css';
 
 //These componenets could also be on a seperate file.
 //modal background
-const Backdrop = (props) => {
-	return <div className={styles.backdrop} />;
+const Backdrop = props => {
+  console.log(props);
+  return <div className={styles.backdrop} onClick={props.onClick} />;
 };
 
 //modal contents
-const ModalOverLay = (props) => {
-	return (
-		<div className={styles.modal}>
-			<div className={styles.content}>{props.children}</div>
-		</div>
-	);
+const ModalOverlay = props => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
 };
 
 const portalElement = document.getElementById('overlays');
 
-const Modal = (props) => {
-	//using a portal
-	return (
-		<Fragment>
-			{ReactDOM.createPortal(<Backdrop />, portalElement)}
-			{ReactDOM.createPortal(
-				<ModalOverLay>{props.children}</ModalOverLay>,
-				portalElement
-			)}
-		</Fragment>
-	);
-	//This will work without using 'portals'
-	// return (
-	// 	<Fragment>
-	// 		<Backdrop />
-	// 		{/* use props.children here to foward the content between modal tags */}
-	// 		<ModalOverLay>{props.children}</ModalOverLay>
-	// 	</Fragment>
-	// );
+const Modal = props => {
+  //using a portal
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClick} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+  //This will work without using 'portals'
+  // return (
+  // 	<Fragment>
+  // 		<Backdrop />
+  // 		{/* use props.children here to foward the content between modal tags */}
+  // 		<ModalOverLay>{props.children}</ModalOverLay>
+  // 	</Fragment>
+  // );
 };
 
 export default Modal;
